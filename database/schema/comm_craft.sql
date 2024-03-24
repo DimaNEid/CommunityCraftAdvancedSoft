2baa00b3a5c50d02285cd77346641c89db8fe2f3
Noor Al-huda Hamayel <nooralhudahamayel7@gmail.com>
2024-03-24 22:59:45 +0200

    collaborations db changes

--- database/schema/comm_craft.sql
+++ database/schema/comm_craft.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 24, 2024 at 09:09 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Mar 24, 2024 at 09:52 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `comm_craft`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrow`
+--
+
+CREATE TABLE `borrow` (
+  `borrow_id` int(50) NOT NULL,
+  `UserID` int(50) NOT NULL,
+  `MaterialID` int(50) NOT NULL,
+  `startDate` date NOT NULL,
+  `endDate` date NOT NULL,
+  `cost` varchar(1000) NOT NULL,
+  `expired` varchar(10) NOT NULL,
+  `returned` varchar(50) NOT NULL DEFAULT 'no',
+  `notified` int(10) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `borrow`
+--
+
+INSERT INTO `borrow` (`borrow_id`, `UserID`, `MaterialID`, `startDate`, `endDate`, `cost`, `expired`, `returned`, `notified`) VALUES
+(2, 2, 1129, '2024-03-22', '2024-04-22', '0.25', 'no', 'no', 0);
 
 -- --------------------------------------------------------
 
@@ -55,7 +80,7 @@ CREATE TABLE `collaborations_users` (
 CREATE TABLE `material` (
   `MaterialID` int(50) NOT NULL,
   `MaterialName` varchar(255) NOT NULL,
-  `Price` varchar(255) NOT NULL,
+  `Price` int(255) NOT NULL,
   `Quantity` int(100) NOT NULL,
   `Available` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -65,8 +90,13 @@ CREATE TABLE `material` (
 --
 
 INSERT INTO `material` (`MaterialID`, `MaterialName`, `Price`, `Quantity`, `Available`) VALUES
-(1112, 'wood', '25$', 3, 'yes'),
-(1113, 'Plastic', '10', 50, '1');
+(1112, 'wood', 25, 49, 'yes'),
+(1113, 'Plastic', 10, 50, '1'),
+(1114, 'marbel', 30, 19, 'yes'),
+(1117, 'glass', 25, 3, 'yes'),
+(1128, 'meter', 25, 2, 'yes'),
+(1129, 'meter1', 25, 1, 'yes'),
+(1131, 'meter3', 25, 3, 'yes');
 
 -- --------------------------------------------------------
 
@@ -135,11 +165,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`UserID`, `Username`, `Password`, `Email`, `FullName`, `Bio`, `Locations`, `Birthdate`, `Gender`, `Phone`, `SocialLinks`) VALUES
-(1, 'dima', '123', 'd@gmail.com', 'dima ', 'hi, ', 'Nablus', '11-10', 'femal', 5487, 'jhfyurnv'),
 (2, 'MARK8', '$2b$10$6Hsm4EVfus5CRj4VLJcqIObC5.JgS8xtiatmsAWPJxk.GsDfAhKbi', 'ghy', 'Mark Sam', 'Designer', 'NewYork', '11/8', 'Male', 578, 'what'),
-(3, 'sally', '$2b$10$ok9on20z9RycO6gceC/g0.GOhGCHDJIXiJgQzkLnJDz5IylanXji6', 'sa.@g', 'sally Sam', 'Designer', 'NewYork', '11/8', 'femal', 578, 'what'),
-(4, 'noor', '$2b$10$gtXo0wiDStwOuvA0gYSr1.OR0OAymev/GJFtwu4nfXA.AQ9iKF1sS', 'noor@mail.com', 'noor', 'bio', 'baita', '07-04-2002', 'Female', 598989898, 'site.com'),
-(5, 'noor', '$2b$10$wuzaSmQP/eO.sTkqNE1H0eklJEa2QNbMPJeZNYLr4XFlIBQklI1c.', 'noor@mail.com', 'noor', 'bio', 'location', '10-10-1010', 'Female', 598989898, 'site.com');
+(3, 'sally', '$2b$10$ok9on20z9RycO6gceC/g0.GOhGCHDJIXiJgQzkLnJDz5IylanXji6', 'sa.@g', 'sally Sam', 'Designer', 'NewYork', '11/8', 'femal', 578, 'what');
 
 -- --------------------------------------------------------
 
@@ -151,13 +178,32 @@ CREATE TABLE `user_res_book` (
   `booking_id` int(50) NOT NULL,
   `UserID` int(50) NOT NULL,
   `MaterialID` int(50) NOT NULL,
-  `MaterialName` varchar(100) NOT NULL,
   `booking_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `user_res_book`
+--
+
+INSERT INTO `user_res_book` (`booking_id`, `UserID`, `MaterialID`, `booking_date`) VALUES
+(4, 2, 1112, '2024-03-22'),
+(5, 2, 1113, '2024-03-22'),
+(6, 2, 1113, '2024-03-22'),
+(8, 2, 1113, '2024-03-22'),
+(9, 2, 1113, '2024-03-22'),
+(12, 2, 1114, '2024-03-22');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `borrow`
+--
+ALTER TABLE `borrow`
+  ADD PRIMARY KEY (`borrow_id`),
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `MaterialID` (`MaterialID`);
 
 --
 -- Indexes for table `collaborations`
@@ -220,20 +266,33 @@ ALTER TABLE `collaborations_users`
   MODIFY `collaboration_user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
+-- AUTO_INCREMENT for table `borrow`
+--
+ALTER TABLE `borrow`
+  MODIFY `borrow_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `UserID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `UserID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user_res_book`
 --
 ALTER TABLE `user_res_book`
-  MODIFY `booking_id` int(50) NOT NULL AUTO_INCREMENT;
+  MODIFY `booking_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `borrow`
+--
+ALTER TABLE `borrow`
+  ADD CONSTRAINT `borrow_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`),
+  ADD CONSTRAINT `borrow_ibfk_2` FOREIGN KEY (`MaterialID`) REFERENCES `material` (`MaterialID`);
 
 --
 -- Constraints for table `user_res_book`
