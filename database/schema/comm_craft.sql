8527bcc754f85e7942dfc13a0c34932b5ac804bb
Noor Al-huda Hamayel <nooralhudahamayel7@gmail.com>
2024-03-24 22:13:11 +0200

    collaborations functionality

---
+++ database/schema/comm_craft.sql
@@ -0,0 +1,248 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 24, 2024 at 09:09 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `comm_craft`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `collaborations`
+--
+
+CREATE TABLE `collaborations` (
+  `collaboration_id` int(50) NOT NULL,
+  `project_id` int(50) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `time` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `collaborations_users`
+--
+
+CREATE TABLE `collaborations_users` (
+  `collaboration_user_id` int(11) NOT NULL,
+  `collaboration_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `material`
+--
+
+CREATE TABLE `material` (
+  `MaterialID` int(50) NOT NULL,
+  `MaterialName` varchar(255) NOT NULL,
+  `Price` varchar(255) NOT NULL,
+  `Quantity` int(100) NOT NULL,
+  `Available` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `material`
+--
+
+INSERT INTO `material` (`MaterialID`, `MaterialName`, `Price`, `Quantity`, `Available`) VALUES
+(1112, 'wood', '25$', 3, 'yes'),
+(1113, 'Plastic', '10', 50, '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE `project` (
+  `ProjectID` int(50) NOT NULL,
+  `Title` varchar(255) NOT NULL,
+  `Description` varchar(500) NOT NULL,
+  `Difficulty` varchar(300) NOT NULL,
+  `Material_id` varchar(255) NOT NULL,
+  `Skills` varchar(500) NOT NULL,
+  `GroupSize` int(100) NOT NULL,
+  `Ranging` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skills_user`
+--
+
+CREATE TABLE `skills_user` (
+  `Skill_id` varchar(255) NOT NULL,
+  `Skill_Name` varchar(255) NOT NULL,
+  `UserID` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE `task` (
+  `TaskID` int(50) NOT NULL,
+  `UserID` int(50) NOT NULL,
+  `TaskName` varchar(255) NOT NULL,
+  `Details` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `UserID` int(50) NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `FullName` varchar(255) NOT NULL,
+  `Bio` varchar(700) NOT NULL,
+  `Locations` varchar(255) NOT NULL,
+  `Birthdate` varchar(50) NOT NULL,
+  `Gender` varchar(50) NOT NULL,
+  `Phone` int(50) NOT NULL,
+  `SocialLinks` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`UserID`, `Username`, `Password`, `Email`, `FullName`, `Bio`, `Locations`, `Birthdate`, `Gender`, `Phone`, `SocialLinks`) VALUES
+(1, 'dima', '123', 'd@gmail.com', 'dima ', 'hi, ', 'Nablus', '11-10', 'femal', 5487, 'jhfyurnv'),
+(2, 'MARK8', '$2b$10$6Hsm4EVfus5CRj4VLJcqIObC5.JgS8xtiatmsAWPJxk.GsDfAhKbi', 'ghy', 'Mark Sam', 'Designer', 'NewYork', '11/8', 'Male', 578, 'what'),
+(3, 'sally', '$2b$10$ok9on20z9RycO6gceC/g0.GOhGCHDJIXiJgQzkLnJDz5IylanXji6', 'sa.@g', 'sally Sam', 'Designer', 'NewYork', '11/8', 'femal', 578, 'what'),
+(4, 'noor', '$2b$10$gtXo0wiDStwOuvA0gYSr1.OR0OAymev/GJFtwu4nfXA.AQ9iKF1sS', 'noor@mail.com', 'noor', 'bio', 'baita', '07-04-2002', 'Female', 598989898, 'site.com'),
+(5, 'noor', '$2b$10$wuzaSmQP/eO.sTkqNE1H0eklJEa2QNbMPJeZNYLr4XFlIBQklI1c.', 'noor@mail.com', 'noor', 'bio', 'location', '10-10-1010', 'Female', 598989898, 'site.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_res_book`
+--
+
+CREATE TABLE `user_res_book` (
+  `booking_id` int(50) NOT NULL,
+  `UserID` int(50) NOT NULL,
+  `MaterialID` int(50) NOT NULL,
+  `MaterialName` varchar(100) NOT NULL,
+  `booking_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `collaborations`
+--
+ALTER TABLE `collaborations`
+  ADD PRIMARY KEY (`collaboration_id`);
+
+--
+-- Indexes for table `collaborations_users`
+--
+ALTER TABLE `collaborations_users`
+  ADD PRIMARY KEY (`collaboration_user_id`);
+
+--
+-- Indexes for table `material`
+--
+ALTER TABLE `material`
+  ADD PRIMARY KEY (`MaterialID`);
+
+--
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`ProjectID`);
+
+--
+-- Indexes for table `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`TaskID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- Indexes for table `user_res_book`
+--
+ALTER TABLE `user_res_book`
+  ADD PRIMARY KEY (`booking_id`),
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `MaterialID` (`MaterialID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `collaborations`
+--
+ALTER TABLE `collaborations`
+  MODIFY `collaboration_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `collaborations_users`
+--
+ALTER TABLE `collaborations_users`
+  MODIFY `collaboration_user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `UserID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user_res_book`
+--
+ALTER TABLE `user_res_book`
+  MODIFY `booking_id` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_res_book`
+--
+ALTER TABLE `user_res_book`
+  ADD CONSTRAINT `user_res_book_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`),
+  ADD CONSTRAINT `user_res_book_ibfk_2` FOREIGN KEY (`MaterialID`) REFERENCES `material` (`MaterialID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
